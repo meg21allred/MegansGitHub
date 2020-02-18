fd74ac02f460238034ec527b29a6e1c8074bafb3
meganallred <meg21allred@gmail.com>
2020-02-17 21:21:54 -0500

    adding to project

--- app/src/main/java/org/byui/meg21allred/megansgithub/MainActivity.java
+++ app/src/main/java/org/byui/meg21allred/megansgithub/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("this is Megan's gitHub!");
+        System.out.println("adding to the git");
     }
 }
